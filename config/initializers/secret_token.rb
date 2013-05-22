936156c351ab804025ed94e87eba08dbe0d72436
Oblivions <jgrado691103@gmail.com>
2013-05-22 00:09:43 -0600

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SampleApp::Application.config.secret_token = '74dee6184d292f8435a216d05f61af996383aabf6ca2999f6162b279c76f6f7fe2b8a7a81c982a9cdb0ca4995bb6b0463ae4eef9c7cc11a4b656a6c7b0da6765'
