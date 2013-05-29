9773ce5d667edc163f115b3abfdd41bfbd5cc4f8
Oblivions <jgrado691103@gmail.com>
2013-05-29 17:01:26 -0600

    Finalizado alta usuarios con version nueva

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,7 +9,7 @@ class UsersController < ApplicationController
   end
   
     def create
-    @user = User.new(user_params)
+    @user = User.new(params[:user])
     if @user.save
       flash[:success] = "Bienvenidos a la aplicacion de Ejemplo!"
       redirect_to @user
--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -3,7 +3,7 @@ module UsersHelper
   # Returns the Gravatar (http://gravatar.com/) for the given user.
   def gravatar_for(user)
     gravatar_id = Digest::MD5::hexdigest(user.email.downcase)
-    gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}"
+    gravatar_url = "https://es.gravatar.com/avatar/80ae1d0c11023518a47492d29d28cf32?s=80/#{gravatar_id}"
     image_tag(gravatar_url, alt: user.name, class: "gravatar")
   end
 end
